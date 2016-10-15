cc7d70e03e123e9b067f861e3982335f71a72a9f
Ikey Doherty <ikey@solus-project.com>
2016-10-15 02:26:08 +0100

    Import a subset of the Budgie theme to provide our 3.20 styling
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ theme/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp        = require("gulp");
+var sass        = require("gulp-sass");
+var filter      = require('gulp-filter');
+
+function buildTheme(version)
+{
+    return gulp.src(version + '/sass/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed',
+            precision: 5,
+            onError: function (err) {
+                notify().write(err);
+            }
+        }))
+        .pipe(gulp.dest(version))
+}
+
+var themeVersions = ['3.20'];
+themeVersions.forEach(function(version) {
+    gulp.task(version, function() {
+        return buildTheme(version);
+    });
+});
+
+gulp.task('default', themeVersions);
